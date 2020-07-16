1fb9ae8ad78f1b643333e8bf04133a1bfadf1b67
MeriemChouk <67969241+MeriemChouk@users.noreply.github.com>
2020-07-16 23:17:06 +0100

    Add files via upload

---
+++ API/TMDBApi.js
@@ -0,0 +1,22 @@
+// API/TMDBApi.js
+
+// API/TMDBApi.js
+const API_TOKEN = "0d3de02e599d0483eba694c9ec8a32f7";
+export function getFilmsFromApiWithSearchedText(text, page) {
+    const url = 'https://api.themoviedb.org/3/search/movie?api_key=' + API_TOKEN + '&language=fr&query=' + text + "&page=" + page
+    return fetch(url)
+        .then((response) => response.json())
+        .catch((error) => console.error(error))
+
+}
+// API/TMDBApi.js
+
+export function getImageFromApi(name) {
+    return 'https://image.tmdb.org/t/p/w300' + name
+}
+ // Récupération du détail d'un film
+export function getFilmDetailFromApi (id) {
+  return fetch('https://api.themoviedb.org/3/movie/' + id + '?api_key=' + API_TOKEN + '&language=fr')
+    .then((response) => response.json())
+    .catch((error) => console.error(error));
+}
---
+++ App.js
@@ -0,0 +1,14 @@
+
+// App.js
+
+import React from 'react';
+//import Search from './Components/Search';
+import Navigation from './Navigation/Navigation';
+
+export default function App() {
+    return (
+       // <Search />
+	   <Navigation/>
+    );
+}
+
